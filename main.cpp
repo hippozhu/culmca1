bacea06afdcce9878d7286f7fb41b243acf28afc
hippozhu <hippozhu@gmail.com>
2013-01-30 19:53:04 -0600

    initial

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "mycommon.h"
+#include "SVMData.h"
+#include "Neighborhood.h"
+#include "KernelNeighborhood.h"
+#include "Solver.h"
+
+int main(int argc, char** argv){
+  string filestem(argv[1]);
+  //int NFeature = atoi(argv[2]);
+  int NClass = atoi(argv[2]);
+  int k = atoi(argv[3]);
+  double mu = atof(argv[4]);
+  double alpha = atof(argv[5]);
+  double nu = atof(argv[6]);
+  
+  SVMData data_train(filestem+".train"); 
+  SVMData data_test(filestem+".test");
+  //cout << "a1" << endl;
+  KernelNeighborhood nb(data_train, data_test, NClass, k);
+  //cout << "t: " << nb.target[0] << " " << nb.target[1] << " " << nb.target[2] << endl;
+  cout << nb.nfeat << "*" << nb.ninst << ", k=" << nb.k << endl;  
+  Solver s(nb, mu, alpha, nu);
+  double *r = (double *)malloc(sizeof(double) * nb.nfeat * nb.ninst);
+  kernelTest(nb.nfeat, nb.ninst, nb.ninst_test, nb.k, r, mu, s.alpha, s.nu);
+  //s.cusolve();
+}
+
